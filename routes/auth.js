fcec2691012d1560f857e7bfdf45fa41b7243dfb
Pongphat.Mit <pongphat.mit@gmail.com>
2025-01-12 00:04:53 +0700

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
-
 dotenv.config();
 
 const authRoutes = (pool) => {
